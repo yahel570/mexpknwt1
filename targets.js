d17e55af36922de92b9e5607a76e9d12566e3508
yahel570 <76042106+yahel570@users.noreply.github.com>
2025-01-30 14:07:03 +0200

    Add files via upload

--- targets.js
+++ targets.js
@@ -4,22 +4,22 @@ function getTarget() {
     var StimSet = ['Chinese character','Painting','Nonword','Chinese character','Painting','Nonword'];
     var preEval = ['With','Without','With','Without','With','Without'];
     var Stimuli = [['Chinese character1.jpg','Chinese character2.jpg','Chinese character3.jpg','Chinese character4.jpg','Chinese character5.jpg','Chinese character6.jpg','Chinese character7.jpg','Chinese character8.jpg','Chinese character9.jpg','Chinese character10.jpg'],['Painting1.jpg','Painting2.jpg','Painting3.jpg','Painting4.jpg','Painting5.jpg','Painting6.jpg','Painting7.jpg','Painting8.jpg','Painting9.jpg','Painting10.jpg'],['Nonword1.jpg','Nonword2.jpg','Nonword3.jpg','Nonword4.jpg','Nonword5.jpg','Nonword6.jpg','Nonword7.jpg','Nonword8.jpg','Nonword9.jpg','Nonword10.jpg'],['Chinese character1.jpg','Chinese character2.jpg','Chinese character3.jpg','Chinese character4.jpg','Chinese character5.jpg','Chinese character6.jpg','Chinese character7.jpg','Chinese character8.jpg','Chinese character9.jpg','Chinese character10.jpg'],['Painting1.jpg','Painting2.jpg','Painting3.jpg','Painting4.jpg','Painting5.jpg','Painting6.jpg','Painting7.jpg','Painting8.jpg','Painting9.jpg','Painting10.jpg'],['Nonword1.jpg','Nonword2.jpg','Nonword3.jpg','Nonword4.jpg','Nonword5.jpg','Nonword6.jpg','Nonword7.jpg','Nonword8.jpg','Nonword9.jpg','Nonword10.jpg']];
-    var T1Filler1Rating = ['-3: Dislike a lot', NA, '-1: Slightly dislike',NA, '1: Slightly like', NA];
-    var T1Filler2Rating = ['-2: Moderately dislike', NA,'0: Neutral', NA, '2: Moderately like', NA];
-    var T1Filler3Rating = ['-1: Slightly dislike',NA,'0: Neutral', NA,'1: Slightly like',NA];
-    var T1Filler4Rating = [ '-3: Dislike a lot',NA, '-1: Slightly dislike',NA,'0: Neutral',NA];
-    var T1Target1Rating = ['-2: Moderately dislike',NA,'0: Neutral',NA, '1: Slightly like', NA];
-    var T1Target2Rating = ['0: Neutral', NA, '1: Slightly like', NA, '3: Like a lot',NA];
-    var T1Target3Rating = ['0: Neutral', NA, '1: Slightly like', NA, '2: Moderately like',NA];
-    var T1Target4Rating = ['0: Neutral', NA, '0: Neutral', NA,'0: Neutral', NA];
-    var T2Filler1Rating = [ '-3: Dislike a lot', '-2: Moderately dislike', '-1: Slightly dislike','0: Neutral', '1: Slightly like', '2: Moderately like', '3: Like a lot'];
-    var T2Filler2Rating = [ '-1: Slightly dislike','0: Neutral', '1: Slightly like', '2: Moderately like', '3: Like a lot','-3: Dislike a lot', '-2: Moderately dislike'];
-    var T2Filler3Rating = [ '-3: Dislike a lot','0: Neutral', '1: Slightly like', '2: Moderately like', '-2: Moderately dislike', '-1: Slightly dislike','3: Like a lot'];
-    var T2Filler4Rating = [  '1: Slightly like', '-3: Dislike a lot', '-1: Slightly dislike','0: Neutral','2: Moderately like', '-2: Moderately dislike', '3: Like a lot'];
-    var T2Target1Rating = [ '1: Slightly like', '2: Moderately like', '-3: Dislike a lot', '-2: Moderately dislike','0: Neutral', '3: Like a lot', '-1: Slightly dislike'];
-    var T2Target2Rating = [ '0: Neutral', '1: Slightly like', '2: Moderately like', '-3: Dislike a lot', '-2: Moderately dislike', '-1: Slightly dislike','3: Like a lot'];
-    var T2Target3Rating = [ '-3: Dislike a lot','1: Slightly like', '-2: Moderately dislike', '2: Moderately like', '-1: Slightly dislike','0: Neutral', '3: Like a lot'];
-    var T2Target4Rating = [ '1: Slightly like', '2: Moderately like', '3: Like a lot', '-3: Dislike a lot', '-2: Moderately dislike', '-1: Slightly dislike','0: Neutral'];
+    var T1Filler1Rating = ['-3: Dislike a lot', 'NA', '-1: Slightly dislike','NA', '1: Slightly like', 'NA'];
+    var T1Filler2Rating = ['-2: Moderately dislike', 'NA','0: Neutral', 'NA', '2: Moderately like', 'NA'];
+    var T1Filler3Rating = ['-1: Slightly dislike','NA','0: Neutral', 'NA','1: Slightly like','NA'];
+    var T1Filler4Rating = [ '-3: Dislike a lot', 'NA', '-1: Slightly dislike','NA','0: Neutral','NA'];
+    var T1Target1Rating = ['-2: Moderately dislike','NA','0: Neutral','NA', '1: Slightly like','NA'];
+    var T1Target2Rating = ['0: Neutral', 'NA', '1: Slightly like', 'NA', '3: Like a lot','NA'];
+    var T1Target3Rating = ['0: Neutral', 'NA', '1: Slightly like', 'NA', '2: Moderately like','NA'];
+    var T1Target4Rating = ['0: Neutral', 'NA', '0: Neutral', 'NA','0: Neutral','NA'];
+    var T2Filler1Rating = [ '-3: Dislike a lot', '-2: Moderately dislike', '-1: Slightly dislike','0: Neutral', '1: Slightly like', '2: Moderately like'];
+    var T2Filler2Rating = [ '-1: Slightly dislike','0: Neutral', '1: Slightly like', '2: Moderately like', '3: Like a lot','-3: Dislike a lot'];
+    var T2Filler3Rating = [ '-3: Dislike a lot','0: Neutral', '1: Slightly like', '2: Moderately like', '-2: Moderately dislike', '-1: Slightly dislike'];
+    var T2Filler4Rating = [  '1: Slightly like', '-3: Dislike a lot', '-1: Slightly dislike','0: Neutral','2: Moderately like', '-2: Moderately dislike'];
+    var T2Target1Rating = [ '1: Slightly like', '2: Moderately like', '-3: Dislike a lot', '-2: Moderately dislike','0: Neutral', '3: Like a lot'];
+    var T2Target2Rating = [ '0: Neutral', '1: Slightly like', '2: Moderately like', '-3: Dislike a lot', '-2: Moderately dislike', '-1: Slightly dislike'];
+    var T2Target3Rating = [ '-3: Dislike a lot','1: Slightly like', '-2: Moderately dislike', '2: Moderately like', '-1: Slightly dislike','0: Neutral'];
+    var T2Target4Rating = [ '1: Slightly like', '2: Moderately like', '3: Like a lot', '-3: Dislike a lot', '-2: Moderately dislike', '-1: Slightly dislike'];
 
 
 
